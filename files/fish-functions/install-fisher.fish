c70996c5b7993fa60950ff896f0d9383c5f14edc
Lukas Gierth <lukas.gierth@posteo.de>
2025-08-19 12:20:37 +0200

    fix: remove functions

--- files/fish-functions/install-fisher.fish
+++
@@ -1,3 +0,0 @@
-function install-fisher --description='Force installs fisher itself'
-    command curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; and fisher install jorgebucaran/fisher
-end
