0dce4c5d06a5c4dcd4dba42e7337c982fd0d371a
Lukas Gierth <lukas.gierth@posteo.de>
2025-08-20 11:42:02 +0200

    feat: allow docker usage by all with sudo and without password; redirect to sudo when run without

---
+++ files/scripts/allow-docker.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Patch docker to not need sudo
+
+# Bash Shell
+cat <<'EOF' >/etc/profile.d/docker.sh
+if [ "$EUID" -ne 0 ]; then
+    docker() {
+        # Check if the command is already running with sudo
+        if [ "$EUID" -eq 0 ]; then
+            /usr/bin/docker "$@"
+        else
+            sudo /usr/bin/docker "$@"
+        fi
+    }
+fi
+EOF
+
+# Fish Shell
+cat <<'EOF' >/usr/share/fish/vendor_conf.d/01-docker.fish
+if test (id -u) -ne 0
+    function docker
+        if test (id -u) -eq 0
+            /usr/bin/docker $argv
+        else
+            sudo /usr/bin/docker $argv
+        end
+    end
+end
+EOF
+
+cat <<'EOF' >/etc/sudoers.d/002-docker
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/docker
+EOF
