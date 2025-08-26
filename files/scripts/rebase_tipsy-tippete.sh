3fd039fd16ac7000894f159bdd48e0efe68903ac
Lukas Gierth <lukas.gierth@posteo.de>
2025-08-26 16:21:07 +0200

    fix: move to services without logic and sh script

---
+++ files/scripts/rebase_tipsy-tippete.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Bash Shell
+cat <<'EOF' >/usr/lib/systemd/system/signed-autorebase.service
+[Unit]
+Description=Autorebase to signed OCI and reboot
+ConditionPathExists=!/etc/autorebase-signed
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=oneshot
+StandardOutput=journal+console
+ExecStart=/usr/bin/rpm-ostree rebase --bypass-driver ostree-image-signed:docker://ghcr.io/lukasgierth/tipsy-tippete:latest
+ExecStart=/usr/bin/touch /etc/autorebase-signed
+ExecStart=/usr/bin/systemctl disable signed-autorebase.service
+
+[Install]
+WantedBy=multi-user.target
+EOF
