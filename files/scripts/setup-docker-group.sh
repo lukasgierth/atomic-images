c46d279003c1f2c1c9d6128150c3567e1db362e8
Lukas Gierth <lukas.gierth@posteo.de>
2025-08-26 18:29:39 +0200

    fix: rename and enable

--- files/scripts/setup-docker-group.sh
+++ files/scripts/setup-docker-group.sh
@@ -3,15 +3,17 @@
 set -euo pipefail
 
 # Bash Shell
-cat <<'EOF' >/usr/lib/systemd/system/setup-docker-groups.service
+cat <<'EOF' >/usr/lib/systemd/system/setup-docker-group.service
 [Unit]
 Description=Set up docker group and add all users to it
+ConditionPathExists=!/etc/setup-docker-group
 After=multi-user.target
 
 [Service]
 Type=oneshot
 ExecStart=/usr/bin/bash -c "getent group docker &> /dev/null || groupadd -r docker; for user in $(getent passwd | grep 'home' | awk -F: '{print $1}'); do usermod -aG docker $user; done"
-ExecStopPost=/usr/bin/systemctl disable --now setup-docker-groups.service
+ExecStart=/usr/bin/touch /etc/setup-docker-group
+ExecStopPost=/usr/bin/systemctl disable --now setup-docker-group.service
 
 [Install]
 WantedBy=multi-user.target
