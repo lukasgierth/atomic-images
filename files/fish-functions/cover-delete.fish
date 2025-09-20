06760fe9fb2423be0adfba2c790fa7e7faf6b974
Lukas Gierth <lukas.gierth@posteo.de>
2025-09-20 23:44:39 +0200

    fix: cover-delete function

--- files/fish-functions/cover-delete.fish
+++ files/fish-functions/cover-delete.fish
@@ -2,10 +2,10 @@ function cover-delete
     for ext in flac opus ogg mp3
         for file in *.$ext
             if test -f $file
-                set tmpfile (mktemp)
+                set tmpfile tmp_$file
                 echo "Processing $file"
                 command ffmpeg -i $file -map 0:a -c:a copy $tmpfile
-                mv $tmpfile $file
+                mv -f $tmpfile $file
             end
         end
     end
