a33c87f4a7742d50a156242ec14d8c8c4ef4800e
Lukas Gierth <lukas.gierth@posteo.de>
2025-09-29 17:41:33 +0200

    feat: also delete repository files

--- files/scripts/disable-layering.sh
+++ files/scripts/disable-layering.sh
@@ -6,7 +6,14 @@ echo "Disable rpm-ostree layering"
 sed -i '/^#*LockLayering=.*/s/.*/LockLayering=true/' /etc/rpm-ostreed.conf
 
 echo "Save package list to image"
-/usr/bin/dnf5 list --installed > /usr/share/dnf5/installed_packages.txt
+/usr/bin/dnf5 list --installed > /usr/share/dnf5/packages_installed.txt
+
+echo "Save repository list to image"
+ls /etc/yum.repos.d > /usr/share/dnf5/repositories_files.txt
+cat /etc/yum.repos.d/* > /usr/share/dnf5/repositories_installed.txt
+
+echo "Delete repositories from image"
+rm /etc/yum.repos.d/*.repo
 
 echo "Disable general usage of dnf & it's symlinks like yum"
 
