5b21cacf84aff6ef2ea6e0ad8c6aa61899acf220
Lukas Gierth <lukas.gierth@ta-systeme.com>
2025-11-10 11:21:09 +0100

    feat: add tools to setup btrfs snapshots with snapper; useful when installed with btrfs

--- files/scripts/hide-applications.sh
+++ files/scripts/hide-applications.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 # Hide applications which should not appear as gui applications to call
-echo 'Hidden=true' >> /usr/share/applications/btop.desktop
-echo 'Hidden=true' >> /usr/share/applications/gcdmaster.desktop
-echo 'Hidden=true' >> /usr/share/applications/nvim.desktop
-echo 'Hidden=true' >> /usr/share/applications/nvtop.desktop
+echo 'Hidden=true' >>/usr/share/applications/btop.desktop
+echo 'Hidden=true' >>/usr/share/applications/gcdmaster.desktop
+echo 'Hidden=true' >>/usr/share/applications/nvim.desktop
+echo 'Hidden=true' >>/usr/share/applications/nvtop.desktop
+echo 'Hidden=true' >>/usr/share/applications/btrfs-assistant.desktop
