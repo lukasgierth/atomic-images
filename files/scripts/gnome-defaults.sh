1f2b7be6ad5f7973c2762bdaa3a7cc5a9c20c1fa
Lukas Gierth <lukas.gierth@posteo.de>
2025-12-15 12:16:37 +0100

    chore: remove gnome keyboard shortcut overrides

--- files/scripts/gnome-defaults.sh
+++ files/scripts/gnome-defaults.sh
@@ -9,26 +9,4 @@ cat <<'EOF' >/etc/gnome-initial-setup/vendor.conf
 skip=software
 EOF
 
-mkdir -p /etc/dconf/db/distro.d
-cat <<'EOF' >/etc/dconf/db/distro.d/01-custom-keybindings
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0]
-name='Open Alacritty'
-command='alacritty'
-binding='<Super><Shift>t'
-
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1]
-name='Open Default Terminal'
-command='ptyxis'
-binding='<Super>t'
-
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2]
-name='Open Firefox'
-command='flatpak run org.mozilla.firefox'
-binding='<Super>b'
-
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3]
-name='Open Files'
-command='nautilus'
-binding='<Super>f'
-EOF
-echo "Compile dconf db now after changes!" && dconf update
+echo "Compile dconf db now after possible changes!" && dconf update
