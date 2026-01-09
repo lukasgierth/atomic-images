4739cadb453a7f221d444ab1332575516481fc8b
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-01-09 12:18:10 +0100

    feat: set custom keybindings again

--- files/scripts/gnome-defaults.sh
+++ files/scripts/gnome-defaults.sh
@@ -9,4 +9,23 @@ cat <<'EOF' >/etc/gnome-initial-setup/vendor.conf
 skip=software
 EOF
 
+mkdir -p /etc/dconf/db/distro.d
+
+cat <<'EOF' >/etc/dconf/db/distro.d/01-custom-keybindings
+[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0]
+name='Open Alacritty'
+command='alacritty'
+binding='<Super>t'
+
+[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1]
+name='Open Files'
+command='nautilus'
+binding='<Super>f'
+
+[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2]
+name='Use screenshotter with gradia'
+command='flatpak run be.alexandervanhee.gradia --screenshot=INTERACTIVE'
+binding='Print'
+EOF
+
 echo "Compile dconf db now after possible changes!" && dconf update
