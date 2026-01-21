192cebbf6a034228555d27fca4862b6bf5e6af70
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-01-21 12:02:32 +0100

    fix: binding, deprecated call in atuin init

--- files/fish-configs/32-atuin.fish
+++ files/fish-configs/32-atuin.fish
@@ -1,5 +1,7 @@
 # only when in interactive shell and the command exists
 if status --is-interactive
     and type -q atuin
-    atuin init fish | source
+    # FIX: revert back when atuin version in fedora is newer again and this has been fixed upstream
+    atuin init fish | tac | sed '1,5s/ -k//' | tac | source
+    # atuin init fish | source
 end
