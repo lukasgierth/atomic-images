c6f64bbb304851c934403a9536f1a81cbb3fab19
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-01-26 17:00:42 +0100

    feat: add

--- files/fish-configs/33-dnf.fish
+++ files/fish-configs/33-dnf.fish
@@ -3,17 +3,21 @@ if status --is-interactive
     function dnf
         echo "Layering disabled, but if you really need to use dnf:"
         echo /usr/bin/dnf
+        exit 1
     end
     function dnf5
         echo "Layering disabled, but if you really need to use dnf5:"
         echo /usr/bin/dnf5
+        exit 1
     end
     function microdnf
         echo "Layering disabled, but if you really need to use microdnf:"
         echo /usr/bin/microdnf
+        exit 1
     end
     function yum
         echo "Layering disabled, but if you really need to use yum:"
         echo /usr/bin/yum
+        exit 1
     end
 end
