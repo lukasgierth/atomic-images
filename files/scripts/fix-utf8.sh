44337bded7246c33db0decf774bcb1b70c28528b
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-01-26 18:18:33 +0100

    fix: also remove the file from niri
    Still will not work most likely because it is in a previous layer...

--- files/scripts/fix-utf8.sh
+++ files/scripts/fix-utf8.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 # Workaround: Rename just's CN readme to README.zh-cn.md
 # otherwise install with iso fails
-mv '/usr/share/doc/just/README.中文.md' '/usr/share/doc/just/README.zh-cn.md'
+mv -f '/usr/share/doc/just/README.中文.md' '/usr/share/doc/just/README.zh-cn.md'
+rm -f '/usr/share/doc/niri/wiki/Layer‐Shell-Components.md'
