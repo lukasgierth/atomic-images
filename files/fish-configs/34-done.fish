1af93f4784720c41c7cfcbfbd081230c5eee9291
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-02-18 14:36:46 +0100

    chore: increase done time to 5min

--- files/fish-configs/34-done.fish
+++ files/fish-configs/34-done.fish
@@ -1,9 +1,9 @@
 # only when in interactive shell and the command exists
 if status --is-interactive
     and type -q notify-send
-    # Send notification when command takes longer than 30s to finish, set CMD_DURATION to change time
+    # Send notification when command takes longer than 5m to finish, set CMD_DURATION to change time
     function notify_long_commands --on-event fish_postexec
-        if test -n "$CMD_DURATION" -a $CMD_DURATION -ge 30000
+        if test -n "$CMD_DURATION" -a $CMD_DURATION -ge 300000
             set -l duration_s (math -s0 "$CMD_DURATION / 1000")
             set -l command_text (string join " " $argv | string sub --length 50)
             test (string length (string join " " $argv)) -gt 50; and set command_text "$command_text..."
