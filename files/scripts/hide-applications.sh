6abeca27b7845a57c658bdf4489ab4f768d9315c
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-05-26 11:18:38 +0200

    fix: gcdmaster does not exist anymore

--- files/scripts/hide-applications.sh
+++ files/scripts/hide-applications.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 # Hide applications which should not appear as gui applications to call
 echo 'Hidden=true' >>/usr/share/applications/aerc.desktop
 echo 'Hidden=true' >>/usr/share/applications/btop.desktop
-echo 'Hidden=true' >>/usr/share/applications/gcdmaster.desktop
 echo 'Hidden=true' >>/usr/share/applications/nvim.desktop
 echo 'Hidden=true' >>/usr/share/applications/nvtop.desktop
 echo 'Hidden=true' >>/usr/share/applications/btrfs-assistant.desktop
