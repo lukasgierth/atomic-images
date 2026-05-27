c38caccbd5ba12cc5f1a9b350e1ee97e34dbaaf1
Lukas Gierth <lukas.gierth@ta-systeme.com>
2026-05-27 17:52:40 +0200

    chore: remove pangolin-cli aliases

--- files/bash-profile/pangolin-cli.sh
+++
@@ -1,8 +0,0 @@
-pangolin-cli() {
-	if [ "$1" = "update" ]; then
-		echo "Updating binary disabled by image maintainer, pangolin-cli is updated with image itself"
-		return 1
-	else
-		command pangolin-cli "$@"
-	fi
-}
--- files/fish-functions/pangolin-cli.fish
+++
@@ -1,8 +0,0 @@
-function pangolin-cli --wraps='pangolin-cli'
-    if test "$argv[1]" = update
-        echo "Updating binary disabled by image maintainer, pangolin-cli is updated with image itself"
-        return 1
-    else
-        command pangolin-cli $argv
-    end
-end
