c7c4fde70d4034173c403b9f004e252d81cd9a3b
Lukas Gierth <lukas.gierth@posteo.de>
2026-05-29 00:20:14 +0200

    feat: add karousel

---
+++ files/scripts/install-karousel.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+UUID="karousel"
+# renovate: datasource=github-releases depName=peterfajdiga/karousel extractVersion=true
+VERSION=0.16
+TMPDIR="/tmp/$UUID"
+EXTDIR="/usr/share/kwin-wayland/scripts/$UUID"
+
+rm -rf "$TMPDIR"
+mkdir -p "$TMPDIR"
+mkdir -p "$EXTDIR"
+cd "$TMPDIR"
+
+curl -L -o "source.tar.gz" "https://github.com/peterfajdiga/karousel/releases/download/v$VERSION/karousel_0_16.tar.gz"
+tar xvf source.tar.gz
+cd $UUID
+cp -r * $EXTDIR/
+
+rm -rf "$TMPDIR"
