672b57a7b6c57e9069d44c372d793021f55ee0e7
Lukas Gierth <lukas.gierth@posteo.de>
2026-05-28 23:58:13 +0200

    feat: add mousetiler

---
+++ files/scripts/install-mousetiler.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+UUID="mousetiler"
+# renovate: datasource=github-releases depName=rxappdev/MouseTiler extractVersion=true
+VERSION=6.3.2
+TMPDIR="/tmp/mousetiler"
+EXTDIR="/usr/share/kwin-wayland/scripts/$UUID"
+
+rm -rf "$TMPDIR"
+mkdir -p "$TMPDIR"
+mkdir -p "$EXTDIR"
+cd "$TMPDIR"
+
+curl -L -o "source.tar.gz" "https://github.com/rxappdev/MouseTiler/archive/refs/tags/v${VERSION}.tar.gz"
+tar xvf source.tar.gz
+cd MouseTiler-$VERSION
+
+cp src/metadata.json $EXTDIR/
+cp -r src/contents $EXTDIR/
+
+rm -rf "$TMPDIR"
