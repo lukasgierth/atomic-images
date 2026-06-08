2734b00cde8eb4229f84d4861fbd34299c26bf16
Lukas Gierth <lukas.gierth@posteo.de>
2026-06-08 12:12:08 +0200

    Update Karousel version to 0.17 in install script

--- files/scripts/install-karousel.sh
+++ files/scripts/install-karousel.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 UUID="karousel"
 # renovate: datasource=github-releases depName=peterfajdiga/karousel extractVersion=true
-VERSION=0.16
+VERSION=0.17
 TMPDIR="/tmp/$UUID"
 EXTDIR="/usr/share/kwin-wayland/scripts/$UUID"
 
@@ -13,7 +13,7 @@ mkdir -p "$TMPDIR"
 mkdir -p "$EXTDIR"
 cd "$TMPDIR"
 
-curl -L -o "source.tar.gz" "https://github.com/peterfajdiga/karousel/releases/download/v$VERSION/karousel_0_16.tar.gz"
+curl -L -o "source.tar.gz" "https://github.com/peterfajdiga/karousel/releases/download/v$VERSION/karousel_0_17.tar.gz"
 tar xvf source.tar.gz
 cd $UUID
 cp -r * $EXTDIR/
