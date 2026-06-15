d652ca7e9fb2c2747af5334cb9edcbf4b1aeb7df
Lukas Gierth <lukas.gierth@posteo.de>
2026-06-15 08:22:13 +0200

    fix: remove my custom keybindings

--- files/scripts/gnome-defaults.sh
+++ files/scripts/gnome-defaults.sh
@@ -13,16 +13,6 @@ mkdir -p /etc/dconf/db/distro.d
 
 cat <<'EOF' >/etc/dconf/db/distro.d/01-custom-keybindings
 [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0]
-name='Open Alacritty'
-command='/usr/bin/alacritty'
-binding='<Super>t'
-
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1]
-name='Open Files'
-command='/usr/bin/nautilus'
-binding='<Super>f'
-
-[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2]
 name='Gradia'
 command='/usr/bin/flatpak run be.alexandervanhee.gradia --screenshot=INTERACTIVE'
 binding='<Ctrl>Print'
